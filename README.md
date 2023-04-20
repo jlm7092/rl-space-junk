2d6fbc6a7fa15d4ec167911323587ac30dd3e573
James May <jlm7092@rit.edu>
2023-04-20 18:48:16 -0400

    moved to stable_baselines3, got camera feed working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rl-space-junk
 Final Project for EEEE-709 Advanced Robotics
+
+
+# Setup
+Copy the "zmqRemoteAPI" folder from CoppeliaSim into the working directory
--- coppeliasim_env.py
+++ coppeliasim_env.py
@@ -1,28 +1,85 @@
 ###############################################################################
 # File Name: coppeliasim_env.py
 # Purpose: Define a simulation environment to allow Reinforcement Learning
-#          training in CoppeliaSim using Pytorch
+#          training in CoppeliaSim
 # Creator: James May
 # Date Created: 4/17/2023
 ###############################################################################
 
 # Environment Levels
-# 0 - Singular static block floating in space
+# 0 - Singular static cubesat floating in space
 # 1 - space junk dataset objects static floating in space
-# 2 - space junk dataset objects with randomized colors static floating in space
-# 3 - space junk dataset objects with randomized colors drifting in space with randomized angular and linear and velocity
+
 
 import cbor
 from zmqRemoteApi import RemoteAPIClient
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
 
-class SpaceJunkEnv:
-    def __init__(self):
-        self.env_level = 0 #level
+
+class SpaceJunkEnv(gym.Env):
+    """Custom Environment that follows gym interface."""
+
+    #metadata = {"render.modes": ["human"]}
+    
+    #### CORE METHODS FOR RL ####
+    
+    def __init__(self,level=0):
+        super().__init__()
+        self.env_level = level #level
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Box()
+        # Example for using image as input (channel-first; channel-last also works):
+        self.observation_space = spaces.Box(low=0, high=255,shape=(360,640,3), dtype=np.uint8)
         self.sim = None
+        self.load()
+
+        
+    def step(self, action): #this is the chonky one
+        reward = 0 #default
+        return observation, reward, done, info
+
+    
+    def reset(self):
+        self.load()
+        observation = getheadimage()
+        return observation  # reward, done, info can't be included
+
+    
+    def close(self):
+        pass
+    
+    
+    
+    
+    #### HELPER METHODS ####
     
     def load(self): #connect to CoppeliaSim & load environment
         client = RemoteAPIClient('localhost',23000)
         self.sim = client.getObject('sim')
         self.sim.loadScene('/home/vlarko/rl-space-junk/space-sim.ttt')
-        
+        if self.level == 0:
+            self.addcubesat([0.5,0,0.25])
+        return 0
 
+        
+    def addcubesat(self,position):
+        sim = self.sim
+        block = sim.createPrimitiveShape(self.sim.primitiveshape_cuboid,[0.05,0.05,0.15],0)
+        sim.setObjectPosition(block,sim.handle_world,position)
+        
+        
+    def getheadimage(self):
+        sim = self.sim
+        cam=sim.getObject("/Sawyer/head_camera")
+        sim.handleVisionSensor(cam)
+        image,resolution=sim.getVisionSensorImg(cam)
+        pixels = list(image)
+        x = np.array(pixels, dtype=np.uint8)
+        array = x.reshape(360,640,3)
+        
+        return array
+ 
